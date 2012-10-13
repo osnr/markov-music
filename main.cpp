41bddf40f5d0617f2dd30a727f04c4d950e60e57
Jan Dlabal <admin@houbysoft.com>
2012-10-13 03:43:38 -0700

    Always write output to out.wav; all command line arguments will be interpreted as files to read in

--- main.cpp
+++ main.cpp
@@ -11,15 +11,12 @@ int main(int argc, char **argv) {
     if (argc == 1) {
         printf("Usage: %s WAV1 ... WAVN\n\tWAVX are WAV files to read from.\n", argv[0]);
         return 1;
-    } else if (argc == 3) {
+    } else {
         vector<int> input;
         for (int i = 1; i < argc; i++) {
             readSamplesFromWAV(input, argv[i]);
         }
-        markovGeneration(input,argv[2],4,250000,10000000);
-    } else {
-        cout << "Hello Hackathon!" << endl;
-        return 0;
+        markovGeneration(input,"out.wav",4,250000,10000000);
     }
 
 }
