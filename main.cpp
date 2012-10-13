dbe503096a29aea45a649b038350ebdc16cdbedd
Jan Dlabal <admin@houbysoft.com>
2012-10-12 19:55:44 -0700

    Added usage notes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-    cout << "Hello Hackathon!" << endl;
-    return 0;
+    if (argc == 1) {
+        printf("Usage: %s WAV1 ... WAVN\n\tWAVX are WAV files to read from.\n", argv[0]);
+        return 1;
+    } else {
+        cout << "Hello Hackathon!" << endl;
+        return 0;
+    }
 }
