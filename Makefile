2383d77adbfa160e5347fb0fb779b980288e6fbd
Jan Dlabal <admin@houbysoft.com>
2012-10-12 19:16:20 -0700

    Added using namespace std; to main.cpp and added markov.o to Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 COPT=-g
 LOPT=-g -lsndfile
-OBJS=main.o readWAV.o
+OBJS=main.o readWAV.o markov.o
 CC=g++
 
 default: markov-music
--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 
+using namespace std;
+
+
 int main(int argc, char **argv) {
     cout << "Hello Hackathon!" << endl;
     return 0;
