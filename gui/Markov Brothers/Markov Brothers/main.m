5cdeeb7dc1f3d6e2f5b2d08a17edf2ac9b60ed6e
Jan Dlabal <admin@houbysoft.com>
2012-10-13 14:09:53 -0700

    Added GUI files

---
+++ gui/Markov Brothers/Markov Brothers/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Markov Brothers
+//
+//  Created by Jan Dlabal on 10/13/12.
+//  Copyright (c) 2012 Jan Dlabal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
